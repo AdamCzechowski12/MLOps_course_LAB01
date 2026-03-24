88978237c13deddfb7d7c1d2baec09ab37593d77
Adam Czechowski <aczechowski12@gmail.com>
2026-03-24 20:08:12 +0100

    Step 4

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
-def main():
-    print("Hello from mlops-course-lab01!")
+import os
+import argparse
+from dotenv import load_dotenv
+from settings import Settings
+
+
+def export_envs(environment: str = "dev") -> None:
+    env_file = f"config/.env.{environment}"  # wskazuje odpowiedni plik .env
+    if not os.path.exists(env_file):
+        raise ValueError(f"Environment file {env_file} does not exist")
+    load_dotenv(env_file)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description="Load environment variables from specified.env file."
+    )
+    parser.add_argument(
+        "--environment",
+        type=str,
+        default="dev",
+        help="The environment to load (dev, test, prod)",
+    )
+    args = parser.parse_args()
+
+    export_envs(args.environment)
+
+    settings = Settings()
+
+    print("APP_NAME: ", settings.APP_NAME)
+    print("ENVIRONMENT: ", settings.ENVIRONMENT)
